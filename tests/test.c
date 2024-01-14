1b1e306f406ea302855213eaad13835a49cfb643
Thales Xisto <thalesxisto@gmail.com>
2024-01-14 18:46:36 +0000

    att

--- tests/test.c
+++ tests/test.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
-
-int	ft_sum(int a, int b)
-{
-	return (a + b);
-}
-
-int ft_sub(int a, int b)
-{
-	return (a - b);
-}
+#include <unistd.h>
+#include <signal.h>
 
 int	main(void)
 {
-	int	(*op[2])(int, int);
-
-	op[0] = ft_sum;
-	op[1] = ft_sub;
-	printf("%d \n", (*op[0])(2, 3));
-	return (0);
+	printf("%d\n", SIGTR)
 }

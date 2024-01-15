199ec5058a6eaeb0eeb03be488b01c1704899c60
Thales Xisto <thalesxisto@gmail.com>
2024-01-15 19:52:42 +0000

    minitalk

---
+++ projects/minitalk/srcs/client.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: txisto-d <txisto-d@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 14:37:38 by txisto-d          #+#    #+#             */
+/*   Updated: 2024/01/15 19:46:56 by txisto-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+#include "libft.h"
+
+int	main(int argc, char **argv)
+{
+	int					pid;
+	char				*message;
+	struct sigaction	sus;
+
+	sus.sa_handler = ft_sus;
+	sigaction(SIGUSR1, &sus, NULL);
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		message = argv[2];
+		ft_send(pid, message);
+	}
+	else if (argc < 3)
+		ft_printf("error_00: lack of arguments.");
+	else
+		ft_printf("error_01: too many arguments.");
+}
+
+void	ft_send(int pid, char *message)
+{
+	int i;
+	unsigned int byte;
+
+	i = 0;
+	while (message[i])
+	{
+		byte = message[i];
+		ft_bitbybit(pid, byte);
+		i++;
+	}
+	ft_bitbybit(pid, 0);
+}
+
+void	ft_bitbybit(int pid, unsigned char byte)
+{
+	int	i;
+	unsigned char save;
+
+	i = 0;
+	save = byte;
+	while (i < 8)
+	{
+		byte = save;
+		byte = byte << i;
+		byte = byte >> (8 - 1);
+		if (!byte)
+		{
+			ft_bitzero(pid);
+		}
+		else
+		{
+			ft_bitone(pid);
+		}
+		i++;
+	}
+}
+
+void	ft_bitone(int pid)
+{
+	kill(pid, SIGUSR2);
+	usleep(50);
+}
+
+void	ft_bitzero(int pid)
+{
+	kill(pid, SIGUSR1);
+	usleep(50);
+}
+
+void	ft_sus(int sig)
+{
+	(void) sig;
+	ft_printf("Message succesfully received\n");
+}
---
+++ projects/minitalk/srcs/server.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: txisto-d <txisto-d@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 10:22:55 by txisto-d          #+#    #+#             */
+/*   Updated: 2024/01/15 19:48:30 by txisto-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+#include "libft.h"
+
+t_message message;
+
+int	main(void)
+{
+	int		pid;
+	struct sigaction zeroact;
+	struct sigaction oneact;
+
+	pid = getpid();
+	zeroact.sa_sigaction = ft_zerohandler;
+	oneact.sa_sigaction = ft_onehandler;
+	zeroact.sa_flags = SA_SIGINFO;
+	oneact.sa_flags = SA_SIGINFO;
+	ft_printf("Server ID: %d\n", pid);
+	message.i = 0;
+	message.byte = 0;
+	message.message = NULL;
+	message.received = 0;
+	message.pid = 0;
+	ft_receive(&zeroact, &oneact);
+	return (0);
+}
+
+void	ft_zerohandler(int sig, siginfo_t *info, void *nothing)
+{
+	(void) sig;
+	(void) nothing;
+
+	if (!message.pid)
+		message.pid = info->si_pid;
+	message.byte = message.byte << 1;
+	message.i++;
+	if (message.i == 8)
+	{
+		if (!message.byte)
+			message.received = 1;
+		else
+			ft_add();
+		message.i = 0;
+		message.byte = 0;
+	}
+}
+
+void	ft_onehandler(int sig, siginfo_t *info, void *nothing)
+{
+	(void) sig;
+	(void) nothing;
+
+	if (!message.pid)
+		message.pid = info->si_pid;
+	message.byte = message.byte << 1;
+	message.byte += 1;
+	message.i++;
+	if (message.i == 8)
+	{
+		ft_add();
+		message.i = 0;
+		message.byte = 0;
+	}
+}
+
+void	ft_add(void)
+{
+	char	*aux;
+	char	*old;
+	size_t	size;
+
+	size = 1;
+	if (message.message)
+		size = ft_strlen(message.message) + 1;
+	aux = (char *) ft_calloc(sizeof(char), size + 1);
+	if (message.message)
+	{
+		old = message.message;
+		ft_memcpy(aux, old, size);
+		free(old);
+	}
+	aux[size - 1] = message.byte;
+	aux[size] = 0;
+	message.message = aux;
+}
+
+void	ft_receive(struct sigaction *zeroact, struct sigaction *oneact)
+{
+	while (1)
+	{
+		sigaction(SIGUSR1, zeroact, NULL);
+		sigaction(SIGUSR2, oneact, NULL);
+		if (message.received == 0)
+			pause();
+		else
+		{
+			ft_printf(message.message);
+			message.received = 0;
+			free(message.message);
+			message.message = NULL;
+			kill(message.pid, SIGUSR1);
+			message.pid = 0;
+		}
+	}
+}
